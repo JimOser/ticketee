531b91e9b92870cf76872e3daf5dfec832b37c98
JimOser <oserj@concentric.net>
2011-08-23 19:01:47 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ticketee::Application.config.secret_token = 'c812e21314860d251841f0f507b90c31f40e3cc7d750d04c793fcce7d01b1b127c9cb1abddd81ed966e3f5971307ea6101c263f32476c8c111e56bf42f2e31f9'
