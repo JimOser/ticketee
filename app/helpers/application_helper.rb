e009c246482192e557bbfe74a41c877777821381
JimOser <oserj@concentric.net>
2011-08-25 10:27:18 -0700

    Added title functionality for show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def title(*parts)
+    unless parts.empty?
+       content_for :title do
+         c = ["Ticketee" , parts]
+         c.join(" - ") unless parts.empty?
+       end
+    end
+  end
 end
+
